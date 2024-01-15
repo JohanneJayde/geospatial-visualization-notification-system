282cbccb98818b0a11dcc20878ebc07022b5e305
JohanneJayde <jmcclenahan@ewu.edu>
2024-01-15 13:53:59 -0800

    added in existing demo project files

---
+++ main.js
@@ -0,0 +1,229 @@
+import "./style.css";
+import EsriJSON from "ol/format/EsriJSON.js";
+import Map from "ol/Map.js";
+import VectorSource from "ol/source/Vector.js";
+import View from "ol/View.js";
+import XYZ from "ol/source/XYZ.js";
+import { Circle, Fill, Stroke, Style } from "ol/style.js";
+import Select from "ol/interaction/Select.js";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
+import { createXYZ } from "ol/tilegrid.js";
+import { fromLonLat, transform } from "ol/proj.js";
+import { tile as tileStrategy } from "ol/loadingstrategy.js";
+import Feature from "ol/Feature.js";
+import OSM from "ol/source/OSM";
+import Point from "ol/geom/Point.js";
+
+/*
+This is serviceUrl. It contains the live fire data that will be draw using open layers
+*/
+const serviceUrl =
+  "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/" +
+  "WFIGS_Incident_Locations_Current/FeatureServer/";
+const layer = 0;
+
+const perimeterServiceUrl =
+  "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/" +
+  "WFIGS_Interagency_Perimeters_Current/FeatureServer/";
+
+const vectorSource = new VectorSource({
+  format: new EsriJSON(),
+  url: function (extent, resolution, projection) {
+    // ArcGIS Server only wants the numeric portion of the projection ID.
+    const srid = projection
+      .getCode()
+      .split(/:(?=\d+$)/)
+      .pop();
+
+    const url =
+      serviceUrl +
+      layer +
+      "/query/?f=json&" +
+      "returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=" +
+      encodeURIComponent(
+        '{"xmin":' +
+          extent[0] +
+          ',"ymin":' +
+          extent[1] +
+          ',"xmax":' +
+          extent[2] +
+          ',"ymax":' +
+          extent[3] +
+          ',"spatialReference":{"wkid":' +
+          srid +
+          "}}"
+      ) +
+      "&geometryType=esriGeometryEnvelope&inSR=" +
+      srid +
+      "&outFields=*" +
+      "&outSR=" +
+      srid;
+
+    return url;
+  },
+  strategy: tileStrategy(
+    createXYZ({
+      tileSize: 512,
+    })
+  ),
+  attributions: "National Interagency Fire Center",
+});
+
+const map = new Map({
+  target: "map",
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+      name: "Open Street Map",
+    }),
+    new VectorLayer({
+      source: vectorSource,
+      name: "Wildfire Data",
+    }),
+  ],
+  view: new View({
+    center: fromLonLat([-120.740135, 47.751076]),
+    zoom: 7,
+  }),
+});
+
+const addressPointStyle = new Style({
+  image: new Circle({
+    radius: 5,
+    fill: new Fill({
+      color: "#000000",
+    }),
+    stroke: new Stroke({
+      color: "#00FF00",
+      width: 1.25,
+    }),
+  }),
+  zIndex: Infinity,
+});
+
+const selectedPoint = new Select({});
+map.addInteraction(selectedPoint);
+
+var keys;
+var pointContent = document.getElementById("selected-point-content");
+var pointName = document.getElementById("selected-point-name");
+
+//create function for getting wildfire details from clicking points on maps
+var displayWildfireInfo = function (pixel) {
+  var feature = map.forEachFeatureAtPixel(
+    pixel,
+    function (feature, layer) {
+      return feature;
+    },
+    {
+      layerFilter: function (layer) {
+        return layer.get("name") === "Wildfire Data";
+      },
+    }
+  );
+
+  if (feature) {
+    pointContent.innerHTML = "";
+    pointName.innerHTML = "Name: ";
+    keys = feature.getKeys();
+
+    pointName.innerHTML += " " + feature.get("IncidentName");
+    for (let i = 0; i < keys.length; i++) {
+      pointContent.innerHTML += keys[i] + ": " + feature.get(keys[i]) + "<br>";
+    }
+  }
+};
+
+var displayAddressInfo = function (pixel) {
+  var feature = map.forEachFeatureAtPixel(
+    pixel,
+    function (feature, layer) {
+      return feature;
+    },
+    {
+      layerFilter: function (layer) {
+        return layer.get("name") === "plotted-points";
+      },
+    }
+  );
+
+  if (feature) {
+    pointContent.innerHTML = "";
+    keys = Object.entries(feature.get("data"));
+
+    for (let [attribute, value] of keys) {
+      pointContent.innerHTML += attribute + ": " + value + "<br>";
+    }
+
+    pointName.innerHTML = "Name: " + feature.get("data")["display_name"];
+  }
+};
+
+var nonLayerPointDisplay = function (pixel) {
+  var feature = map.forEachFeatureAtPixel(pixel, function (feature, layer) {
+    return feature;
+  });
+
+  if (!feature) {
+    pointContent.innerHTML = "NA";
+    pointName.innerHTML = "Name: NA";
+  }
+};
+
+map.on("singleclick", function (evt) {
+  var pixel = evt.pixel;
+  nonLayerPointDisplay(pixel);
+  displayWildfireInfo(pixel);
+  displayAddressInfo(pixel);
+});
+
+//create layers where adress points are used
+const vectorLayer = new VectorLayer({
+  name: "plotted-points",
+  style: addressPointStyle,
+});
+vectorLayer.setSource(new VectorSource({}));
+
+map.addLayer(vectorLayer);
+
+var addAddressPoint = async function (event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const asString = new URLSearchParams(formData).toString();
+
+  const data = await fetch(
+    "https://nominatim.openstreetmap.org/search?" +
+      asString +
+      "&format=json&limit=1"
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      return json;
+    });
+
+  if (data.length === 0) {
+    return;
+  }
+
+  let plottedPoints = map
+    .getLayers()
+    .getArray()
+    .find((layer) => layer.get("name") == "plotted-points");
+
+  plottedPoints.getSource().addFeature(
+    new Feature({
+      geometry: new Point(fromLonLat([data[0].lon, data[0].lat])),
+      data: data[0],
+    })
+  );
+};
+
+var structuredQueryForm = document.getElementById(
+  "structured-address-lookup-form"
+);
+structuredQueryForm.addEventListener("submit", addAddressPoint, true);
+
+var freeFormQueryForm = document.getElementById(
+  "free-form-address-lookup-form"
+);
+freeFormQueryForm.addEventListener("submit", addAddressPoint, true);
