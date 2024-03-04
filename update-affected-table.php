985cb551c3214265679cd5af07072ba11160abc4
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-03-03 20:56:36 -0800

    removed extra echo for checking data

--- update-affected-table.php
+++ update-affected-table.php
@@ -9,15 +9,8 @@ for ($i = 0; $i < count($data); $i++) {
     for ($j = 0; $j < count($data[$i]["distances"]); $j++){
         $query = "INSERT INTO affected (user_id, fire_id, distance)
         VALUES ({$data[$i]["distances"][$j]["id"]}, '{$data[$i]["wildfireIrwinID"]}', {$data[$i]["distances"][$j]["distance"]})";
-        echo $query;
         $result = $conn->query($query);
 
-        if ($result->num_rows > 0) {
-            echo "didnt' work";
-        }
-        else{
-            echo "Did work";
-        }
     };
 
 $conn->close();
