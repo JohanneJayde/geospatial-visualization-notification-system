543df09301f45b4ab19a5d10ea59b28837a37854
randypipkins <122407752+randypipkins@users.noreply.github.com>
2024-03-17 17:01:23 -0700

    Add files via upload
    Commit all CSV import/export/view files, login frontend and functionality, registration frontend and functionality, and logout functionality for log out button

---
+++ importCSV.php
@@ -0,0 +1,75 @@
+<?php
+// Load the database configuration file
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "CSCD488_490GroupProject";
+$dbname = "testdb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST['importSubmit'])){
+
+    // Allowed mime types
+    $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+
+    // Validate whether selected file is a CSV file
+    if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)){
+
+        // If the file is uploaded
+        if(is_uploaded_file($_FILES['file']['tmp_name'])){
+
+            // Open uploaded CSV file with read-only mode
+            $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
+
+            // Skip the first line
+            fgetcsv($csvFile);
+
+            // Parse data from CSV file line by line
+            while(($line = fgetcsv($csvFile)) !== FALSE){
+                // Get row data
+                $id = $line[0];
+                $name   = $line[1];
+                $email  = $line[2];
+                $street  = $line[3];
+                $city = $line[4];
+                $county = $line[5];
+                $state = $line[6];
+                $country = $line[7];
+                $postalcode = $line[8];
+
+                // Check whether member already exists in the database with the same email
+                $prevQuery = "SELECT id FROM service_members WHERE email = '".$line[1]."'";
+                $prevResult = $conn->query($prevQuery);
+
+                if($prevResult->num_rows > 0){
+                    // Update member data in the database
+                    $conn->query("UPDATE service_members SET name = '" . $name . "', 
+                        street = '" . $street . "', city = '" . $city . "', county = '" . $county ."', state = '" . $state . "',
+                        country = '" . $country . "', postalcode = '" . $postalcode . "', email = '". $email ."' WHERE id = '" . $id . "'");
+                }else{
+                    // Insert member data in the database
+                    $conn->query("INSERT INTO service_members (name, email, street, city, county, state, country, postalcode)
+                        VALUES ('" . $name . "', '" . $email . "', '" . $street . "', '" . $city . "', '" . $country . "', 
+                        '" . $state . "', '" . $country . "', '" . $postalcode . "')");
+                }
+            }
+
+            // Close opened CSV file
+            fclose($csvFile);
+
+            $qstring = '?status=succ';
+        }else{
+            $qstring = '?status=err';
+        }
+    }else{
+        $qstring = '?status=invalid_file';
+    }
+}
+
+// Redirect to the listing page
+header("Location: CSV.php".$qstring);
---
+++ login.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * File for user login
+ * Will be used in index.html
+ */
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "CSCD488_490GroupProject";
+$dbname = "testdb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//Check if the data from the login form was submitted
+if(!isset($_POST['email'], $_POST['password'])) {
+    exit('Please enter username and/or password');
+}
+
+//Prepare SQL statement
+if($stmt = $conn->prepare('SELECT id, password FROM users WHERE email = ?')) {
+    $stmt->bind_param('s', $_POST['email']);
+    $stmt->execute();
+    //Store results so we can check if the account exists in the database
+    $stmt->store_result();
+
+    if($stmt->num_rows > 0) {
+        //Account exists
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        //Verify password
+        if(password_verify($_POST['password'], $password)) {
+            //Password verification success
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['email'];
+            $_SESSION['id'] = $id;
+            header('Location: CSV.php');
+        } else {
+            //Incorrect password
+            echo 'Incorrect password';
+        }
+    } else {
+        //Incorrect username
+        echo 'Incorrect username ';
+    }
+
+    $stmt->close();
+}
